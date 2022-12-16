dd7341a288bd418efe6eeb312a910a857046855f
sunilinus12 <www.suniljaunwal2@gmail.com>
2022-12-16 13:24:56 +0530

    added the fourscreen data

--- src/navigation/MyStack.js
+++ src/navigation/MyStack.js
@@ -10,11 +10,36 @@ export default function MyStack() {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="first" component={FirstScreen} />
-        <Stack.Screen name="second" component={SecondScreen} />
-        <Stack.Screen name="third" component={ThirdScreen} />
-        <Stack.Screen name="fourth" component={FourthScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShadowVisible: false,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontSize: 15,
+            color: 'black',
+            fontFamily: 'Roboto',
+          },
+        }}>
+        <Stack.Screen
+          name="first"
+          options={{title: 'First Screen'}}
+          component={FirstScreen}
+        />
+        <Stack.Screen
+          name="second"
+          options={{title: 'Second Screen'}}
+          component={SecondScreen}
+        />
+        <Stack.Screen
+          name="third"
+          options={{title: 'Third Screen'}}
+          component={ThirdScreen}
+        />
+        <Stack.Screen
+          name="fourth"
+          options={{title: 'Fourth Screen'}}
+          component={FourthScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
